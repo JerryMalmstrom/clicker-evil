483abf3679e36628a535efa956fc0f528626b5f9
Malmstrom <jerry.malmstrom@borealisgroup.com>
2019-04-03 16:17:39 +0200

    ok

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const GAME_FPS = 10;
 
 
 var current_money = START_MONEY;
-var expense_per_second = 7.0;
+var expense_per_second = 0.0;
 
 class Item {
     constructor(name, cost, inc_per_click, inc_per_sec) {
@@ -14,7 +14,12 @@ class Item {
     }
 }
 
-var items = [new Item("Fix Computer", 0, 10, 0), new Item("Set up backup-system", 0, 100, 0)]
+var items = [
+    new Item("Mine Coal", 0, 10, 0),
+    new Item("Fetch Water", 0, 100, 0),
+    new Item("Dig hole", 0, 10, 0),
+    new Item("Cut Stone", 0, 100, 0)
+]
 
 
 
@@ -28,7 +33,35 @@ function initialize() {
         current_money -= expense_per_second;
     }, 1000 );
 
-
+    items.foreach(item => {
+        var htmlcard = '<div id="' + item.name + '" class="card">' +
+        '<div class="content">' +
+        '    <div class="header"' + item.name + '</div>' +
+        '    <div class="description">' +
+        '        Item description' +
+        '    </div>' +
+        '</div>' +
+        '<button class="ui bottom attached button" value="' + item.inc_per_click + '">' +
+        '    <i class="tencent weibo icon"></i>' +
+        '    <span>' + item.name + '</span>' +
+        '</button>' +
+        '</div>"';
+        
+        $('#cards').append(htmlcard);
+    });
+    
+    <div id="item01" class="card">
+                    <div class="content">
+                        <div class="header">Mine</div>
+                        <div class="description">
+                            Item description
+                        </div>
+                    </div>
+                    <button class="ui bottom attached button" value="1">
+                        <i class="tencent weibo icon"></i>
+                        <span>Work in mine</span>
+                    </button>
+                </div>
 
     $( 'button' ).on('click', function() {
         var $button = $(this);
